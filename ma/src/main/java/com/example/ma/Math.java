c3a266a1deea25cf678032e43c165659e42ae563
bilalidrees <bilal.idrees@vectracom.com>
2021-03-08 13:40:49 +0500

    2

---
+++ ma/src/main/java/com/example/ma/Math.java
@@ -0,0 +1,15 @@
+package com.example.ma;
+
+public class Math {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
+    public static int mult(int a, int b) {
+        return a * b;
+    }
+}
